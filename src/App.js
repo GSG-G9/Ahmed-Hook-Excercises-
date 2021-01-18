607a8e27b61655d8d81e0a84fd6ca01ec0dee420
Ahmed-aed <aeid767@gmail.com>
2021-01-19 01:53:40 +0200

    soluation Exercise-4

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Component from './Exercise-1'
 import Palette from './Exercise-2'
 import ToDo from './Exercise-3'
+import SignUp from './Exercise-4'
 function App() {
   return (
     <div className="App">
      <Component/>
      <Palette/>
      <ToDo/>
+     <SignUp/>
     </div>
   );
 }
